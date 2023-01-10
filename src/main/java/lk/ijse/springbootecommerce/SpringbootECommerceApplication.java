c3d443fe2e60a7a8b74200f0607ef216be8d32fb
Rashmi <rashmijayasekara00@gmail.com>
2023-01-10 13:33:04 +0530

    Set up the Spring Boot Project

---
+++ src/main/java/lk/ijse/springbootecommerce/SpringbootECommerceApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.springbootecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/springbootecommerce/SpringbootECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.springbootecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
